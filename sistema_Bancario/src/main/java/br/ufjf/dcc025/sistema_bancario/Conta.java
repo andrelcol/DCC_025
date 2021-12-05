b5224cabd61fe0e28e0aaa0705db73c79b50f18b
André Luiz Cunha de Oliveira <54187669+andrelcol@users.noreply.github.com>
2021-12-05 17:15:33 -0300

    adicionado projeto inicial

---
+++ sistema_Bancario/src/main/java/br/ufjf/dcc025/sistema_bancario/Conta.java
@@ -0,0 +1,11 @@
+package br.ufjf.dcc025.Sistema_Bancario;
+
+/**
+ * @author André Luiz Cunha de Oliveira 201935020;
+ * @author Guilherme Diniz Queiroz 201935008;
+ * @author João Victor Wayand de Jesus Dias 201935012;
+ */
+
+public class Conta {
+    
+}
